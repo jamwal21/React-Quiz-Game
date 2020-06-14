50d26cfe2b9e0f35f3eef9b75a68439f1974233b
Abhinav Jamwal <abhinavjamwal302@gmail.com>
2020-06-14 10:19:15 +0530

    Updates

---
+++ precache-manifest.9444a2e0df7d14c06dd04c4339d87229.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac1c1edbf204c04544331ccedb6f01a",
+    "url": "/React-Quiz-Game/index.html"
+  },
+  {
+    "revision": "b668ea72c62941a51c91",
+    "url": "/React-Quiz-Game/static/css/main.b2e681f2.chunk.css"
+  },
+  {
+    "revision": "249d9a876d0bd3eccf39",
+    "url": "/React-Quiz-Game/static/js/2.b3450ee3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Quiz-Game/static/js/2.b3450ee3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b668ea72c62941a51c91",
+    "url": "/React-Quiz-Game/static/js/main.d15e1a91.chunk.js"
+  },
+  {
+    "revision": "b749844b93d4f88ad3c7",
+    "url": "/React-Quiz-Game/static/js/runtime-main.8da7ba04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Quiz-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
